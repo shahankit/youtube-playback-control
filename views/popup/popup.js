c295cd52d2c5bcb9c272ced2d40b637199d686e7
Ankit Shah <shahankit2313@gmail.com>
2016-01-31 14:10:43 +0530

    Updated UI and added jump and close tab functionality

--- views/popup/popup.js
+++ views/popup/popup.js
@@ -3,32 +3,45 @@ var queryInfo = {url: matchUrl};
 chrome.tabs.query(queryInfo, function(tabs) {
 	var cl = $('#content-list');
 	tabs.forEach(function(tab) {
-		var imageName = tab.audible ? 'pause.png' : 'play.png';
+		var imageClass = tab.audible ? 'pauseImage' : 'playImage';
 		var x = `
 			<div class="item tab open" id=${tab.id}>
-				<div class="tabimage">
-					<img src="/assets/${imageName}" width="16" height="16" border="0">
+				<div class="tabimage" id="playpause${tab.id}">
+					<img class="${imageClass}" id="playImage${tab.id}" border="0">
 				</div>
-				<div>
+				<div class="tabInfo" id="jump${tab.id}">
+					<div class="close" id="close${tab.id}" title="close tab"></div>
 					<div class="title">${tab.title}</div>
 					<div class="url">${tab.url}</div>
 				</div>
 			</div>
 		`
 		cl.append($(x));
-		$('#'+tab.id).on('click', {tabId: tab.id}, function(event) {
+
+		$('#playpause'+tab.id).on('click', {tabId: tab.id}, function(event) {
 			console.log('Clicked tab with event state ', event.data.tabId);
 			chrome.tabs.sendMessage(event.data.tabId, {message: 'toggle_video_state', tabId: event.data.tabId}, function(response) {
 				if (response.error) {
 					console.log('No video found in tab');
 				} else {
 					if (response.paused) {
-						$('#' + response.tabId + ' img').attr('src', '/assets/play.png');
+						$('#playpause' + response.tabId + ' img').attr('class', 'playImage');
 					} else {
-						$('#' + response.tabId + ' img').attr('src', '/assets/pause.png');
+						$('#playpause' + response.tabId + ' img').attr('class', 'pauseImage');
 					}
 				}
 			});
+		});
+
+		$('#jump'+tab.id).on('click', {tabId: tab.id, windowId: tab.windowId}, function(event) {
+			console.log('Clicked tab with event state ', event.data.tabId);
+			chrome.windows.update(event.data.windowId, {focused: true});
+			chrome.tabs.update(event.data.tabId, {active: true});
+		});
+
+		$('#close'+tab.id).on('click', {tabId: tab.id}, function(event) {
+			console.log('Clicked tab with event state ', event.data.tabId);
+			chrome.tabs.remove(event.data.tabId);
 		});
 	});
 	if (tabs.length == 0) {
